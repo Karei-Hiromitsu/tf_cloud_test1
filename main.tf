1b1a1a0b712544188d35ea54ebbc35a393d3654e
Hiromitsu Karei <flatfish0122@gmail.com>
2022-09-18 14:51:22 +0900

    use variables

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 provider "aws" {
 	profile = "terraform"
 	region = "ap-northeast-1"
+	access_key = var.access_key
+	secret_key = var.secret_key
 }
 
 resource "aws_instance" "hello-world" {
 	ami = "ami-0f36dcfcc94112ea1"
 	instance_type = "t2.micro"
 	tags = {
-		Name = "FirstTerraformCloud"
+		Name = var.tag_name
 	}
 
 	user_data = <<EOF
